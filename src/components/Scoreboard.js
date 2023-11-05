a2406aebba8c007b66258d1b150de106093934b0
Anthony McKinney <amv493@gmail.com>
2023-11-05 12:37:32 -0500

    Completed Tic Tac Toe Game

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Scoreboard.css"
+
+export const Scoreboard = ({scores, xPlaying}) => {
+    const {xScore, oScore} = scores;
+    return (
+        <div className="scoreboard">
+            <span className={`score x-score ${!xPlaying && "inactive"}`}>X - {xScore}</span>
+            <span className={`score o-score ${xPlaying && "inactive"}`}>O - {oScore}</span>
+            </div>
+  )
+}
